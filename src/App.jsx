3575ed6c20ba092955a6a0c97f977445de2ba221
Sapphire Smith <ssapphire527@gmail.com>
2023-04-10 18:05:39 +0530

    backgroud color chnge added and completed almost functions and methods ft:onscreen

--- src/App.jsx
+++ src/App.jsx
@@ -17,30 +17,42 @@ const App = () => {
     const fetchWeather = async () => {
       await getFormattedWeatherData({ ...query, units }).then((data) => {
         setWeather(data);
-        console.log(data);
       })
     }
     fetchWeather()
   }, [query, units])
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700"
+
+    const threshold = units === "metric" ? 40: 50;
+
+    console.log(weather.temp);
+    console.log(weather.temp <= threshold);
+    if (weather.temp <= threshold)
+      return "from-cyan-700 to-blue-700"
+
+    return "from-yellow-700 to-orange-700"
+  }
+
 
 
 
 
 
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5  px-32
-    bg-gradient-to-br from-cyan-700 to-blue-700
-    h-fit shadow-md shadow-gray-500'>
-      <NavButtons />
-      <Inputs />
+    <div className={`mx-auto max-w-screen-md mt-4 py-5  px-32
+    bg-gradient-to-br h-fit shadow-md shadow-gray-500 ${formatBackground()}`}>
+      <NavButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} setUnits={setUnits} unit={units} />
 
       {weather && (
         <div>
-          <TimeAndLocation weather={weather}/>
-          <TemperatureAndDetails weather={weather}/>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
 
           <Forecast title="Hourly Forecast" items={weather.hourly} />
-          <Forecast title="Daily Forecast" items={weather.daily}/>
+          <Forecast title="Daily Forecast" items={weather.daily} />
         </div>
       )}
 
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,27 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from '../assets/search.svg'
 import { UilSearch, UilLocationPinAlt } from '@iconscout/react-unicons'
-const Inputs = () => {
+const Inputs = ({ setQuery, setUnits, unit }) => {
+
+  const [city, setCity] = useState('');
+
+  const handleSearchClick = () => {
+    if (city !== '') setQuery({ q: city })
+  }
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({
+          lat,
+          lon
+        })
+      })
+    }
+  }
+
+  const handleUnitChange = (e)=>{
+    const selectedUnit = e.currentTarget.name;
+    if(unit !== selectedUnit) setUnits(selectedUnit)
+  }
+
+
   return (
     <div className='flex flex-row justify-center my-6'>
       <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
         <input
+          value={city}
+          onChange={(e) => {
+            setCity(e.currentTarget.value)
+          }}
           type="text"
           placeholder='Search for city...'
           className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'
         />
         <UilSearch
           size={35}
-          className='text-white cursor-pointer transition ease-out hover:scale-125' />
+          className='text-white cursor-pointer transition ease-out hover:scale-125'
+          onClick={handleSearchClick}
+        />
         <UilLocationPinAlt
           size={35}
-          className='text-white cursor-pointer transition ease-in-out hover:scale-125' />
+          className='text-white cursor-pointer transition ease-in-out hover:scale-125'
+          onClick={handleLocationClick}
+        />
       </div>
 
       <div className=' flex flex-row w-1/4 items-center justify-center'>
-        <button className='metric text-xl text-white font-light'>°C</button>
+        <button
+        name='metric'
+        className='metric text-xl text-white font-light transition ease-out hover:scale-125'
+        onClick={handleUnitChange}
+        >°C</button>
         <p className='text-xl text-white mx-1'>|</p>
-        <button className='imperial text-xl text-white font-light'>°F</button>
+        <button 
+        name='imperial'
+        className='imperial text-xl text-white font-light transition ease-out hover:scale-125'
+        onClick={handleUnitChange}
+        >°F</button>
       </div>
     </div>
   )
